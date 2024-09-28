a3d5766c14ef9b88ae00906ff0f4262efd174d3e
tanurajchaudhari <tanuraj169179@gmail.com>
2024-09-28 20:13:03 +0530

    first

---
+++ classapps/insta_stories/lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title:"insta",
+      home: HomeScreen(),      
+      
+    );
+  }
+}
+class HomeScreen extends StatelessWidget
